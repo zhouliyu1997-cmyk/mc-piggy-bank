a135227fe5a6e4eace30b6b32b6960c8f117fa1b
zhouliyu1997-cmyk <zhouliyu1997@gmail.com>
2026-09-15 23:06:32 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MC_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE"
+  SUPABASE_URL: "https://dooamxxhslkytfphadss.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_J6ILkFosLd8vJ2QHJwJasQ_XGYZRZQd"
 };
